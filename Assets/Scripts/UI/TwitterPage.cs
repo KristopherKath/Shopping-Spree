5271ddfb841207b983beb20a5b8c04ea84863ed7
Kristopher Kath <Kristopher.Kath@gmail.com>
2021-10-04 14:39:52 -0700

    Added twitter link

---
+++ Assets/Scripts/UI/TwitterPage.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwitterPage : MonoBehaviour
+{
+
+
+    public void OpenTwitter()
+    {
+        Application.OpenURL("https://twitter.com/monkybusines0");
+    }
+}
